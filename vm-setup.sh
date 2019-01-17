a069f70689597d71e60d2f0704772749d371f291
Cataldo Azzariti <cataldo.azzariti@sapient.com>
2019-01-17 13:19:20 +0000

    improved VM script and added pyspark file

--- vm-setup.sh
+++ vm-setup.sh
@@ -20,3 +20,15 @@ sudo chown -R ubuntu $SPARK_HOME
 
 cp ./spark-env.sh $SPARK_HOME/conf/spark-env.sh
 cp ./slaves $SPARK_HOME/conf/slaves
+
+# Install Python and other stuff needed
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+sudo python -m pip install Jupiter
+sudo ln -s /usr/bin/python3 /usr/bin/python
+
+# Get over amended pyspark script to have Python paths set up inside
+cp ./pyspark $SPARK_HOME/bin/pyspark
+
+# Get jupyter config
+jupyter notebook --generate-config
